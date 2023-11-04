7a49978f97d3cbc77f3f656d97d66fd85bf748b0
Joal1291 <jordan.albert@github.com>
2023-11-04 08:09:40 +0100

    mise en place arborescence

---
+++ idealadvisor/app/(dashboard)/acceuil/page.jsx
@@ -0,0 +1,7 @@
+export default function Acceuil() {
+    return(
+        <main>
+
+        </main>
+    )
+}
---
+++ idealadvisor/app/components/LogoPrincipal.jsx
@@ -0,0 +1,9 @@
+export default function LogoPrincipal() {
+    return(
+        <>
+            <div>
+
+            </div>
+        </>
+    )
+}
